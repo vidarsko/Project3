076d39eee5021d094fd28bbca3d137b65d132778
vidarsko <vidarsko@gmail.com>
2014-12-07 21:58:47 +0100

    Delete test_compare.m

--- test_compare.m
+++
@@ -1,7 +0,0 @@
-%test compare
-alpha = csvread('test_alpha.csv');
-beta = csvread('test_beta.csv');
-energy_diff = csvread('energy_diff.csv');
-surf(beta,alpha,energy_diff);
-xlabel('beta')
-ylabel('alpha')
