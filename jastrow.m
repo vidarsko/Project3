102d3f762ff30fbd27953f1990efe6dc55d4e56b
vidarsko <vidarsko@gmail.com>
2014-12-07 21:56:09 +0100

    Delete jastrow.m

--- jastrow.m
+++
@@ -1,8 +0,0 @@
-%jastrow
-alpha = csvread('jastrow_alpha.csv');
-beta = csvread('jastrow_beta.csv');
-energy =csvread('jastrow_energies.csv');
-
-surf(beta,alpha,energy)
-xlabel('beta')
-ylabel('alpha')
